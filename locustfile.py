4c18126bbd0bd58b1215e8f4976f75594f3342ff
Lucas <lluc23@hotmail.com>
2025-07-19 17:19:55 +0200

    feat: create function to create random payloads

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,40 @@
 import random
 
-from locust import HttpUser, constant_pacing, env, events, tag, task
+from locust import HttpUser, constant_pacing, env, events, task
 from locust_helpers import TestSetUp, add_args_to_parser
 
+from datetime import date
+from bita.application import SecurityValue
 
+def all_security_values():
+    while True:
+        for e in SecurityValue:
+            yield e.value
+
+def all_weighting_methods(d: str):
+    lb = round(random.uniform(0.01, 1.0), 3) if random.choice((True, False)) else None
+    ub = round(random.uniform(0.01, 1.0), 3) if random.choice((True, False)) else None
+    return {"d": d, "lb": lb, "ub": ub}
+
+def all_filters(d: str):
+    if random.choice((True, False)):
+        n = random.randint(1, 10)
+        return {"n": n, "d": d}
+    p = round(random.uniform(0.01, 100.0), 2)
+    return {"p": p, "d": d}
+
+def all_calendar_rules(dates: list[str]):
+    if random.choice((True, False)):
+        return {"dates": random.sample(dates, k=random.randint(1, len(dates)))}
+    return {"initial_date": random.choice(dates)}
+
+def generate_all_payload_combinations(dates: list[str]):
+    for d in all_security_values():
+        yield {
+            "calendar_rule":all_calendar_rules(dates),
+            "backtest_filter": all_filters(d),
+            "weighting_method": all_weighting_methods(d),
+        }
 
 @events.init_command_line_parser.add_listener
 def _(parser):
@@ -18,17 +49,13 @@ def _(environment: env.Environment, **_):
 
 class User(HttpUser):
     wait_time = constant_pacing(1)
-    host = "http://0.0.0.0:8080"
+    host = "http://localhost:8000"
+    url = "/backtest"
 
     def on_start(self):
-        self.urls = URLS
+        dates = [str(date(year=year,month=random.randint(1, 12),day=random.randint(1, 28))) for year in range(2020, 2025)]
+        self.payload = generate_all_payload_combinations(dates)
 
-    @tag("good_requests")
     @task
     def good_requests(self):
-        self.client.get(url=random.choice(self.urls))
-
-    @tag("bad_requests")
-    @task
-    def bad_requests(self):
-        self.client.get(url=random.choice(self.urls))
+        self.client.post(url=self.url, json=next(self.payload))
